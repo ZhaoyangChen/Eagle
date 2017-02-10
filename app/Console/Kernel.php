61f440c9235dc91cc31f57bf14d1e347047d85d9
陈赵阳 <chenzhaoyang@baixing.com>
2017-02-10 17:37:42 +0800

    tick test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         'App\Console\Commands\GetCitiesInfo',
+        'App\Console\Commands\MinuteTick',
     ];
 
     /**
@@ -29,6 +30,7 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
 
         $schedule->command('eagle:hunt')->daily();
+        $schedule->command('eagle:tick')->everyMinute();
     }
 
     /**
