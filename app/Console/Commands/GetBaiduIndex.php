c2becefef48d8841df5e1b9485b31f123eef46fe
陈赵阳 <chenzhaoyang@baixing.com>
2017-05-12 12:36:51 +0800

    fix indexs

--- app/Console/Commands/GetBaiduIndex.php
+++ app/Console/Commands/GetBaiduIndex.php
@@ -61,10 +61,11 @@ class GetBaiduIndex extends Command
                     $node->_id = $domain . '-' . $rule;
                     $node->domain = $domain;
                     $node->rule = $rule;
-                    $node->indexs = [];
-                    foreach ($res_obj->list_new as $arr) {
-                        $node->indexs[$arr['ctime']] = intval($arr['total']);
+                    $tmpIndex = [];
+                    foreach ($res_obj->list_new as $o) {
+                        $tmpIndex[$o->ctime] = $o->total;
                     }
+                    $node->indexs = $tmpIndex;
                     $node->updatedTime = time();
                     $former = Eagle_index::find($node->_id);
                     if ($former) {
